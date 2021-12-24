e9545055e53473e729e293206ebacf258c227a09
MajorChe <charithnanduri03@gmail.com>
2021-12-24 15:16:40 -0500

    login page added

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ app.get('/',(req,res) => {
   res.render("index");
 });
 
+app.get('/login',(req,res) => {
+  res.render("login");
+});
+
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`)
 })
