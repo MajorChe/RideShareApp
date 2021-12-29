629acee8d399b40691196847817043a9b29c8bca
kaviramsv <kavitasvenkitachalam@gmail.com>
2021-12-29 01:42:05 +0000

    Modify schema for date time and image field and add more seeds to rides table

--- db/schema/02_rides.sql
+++ db/schema/02_rides.sql
@@ -4,12 +4,14 @@ CREATE TABLE rides (
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   origin VARCHAR(255) NOT NULL,
   destination VARCHAR(255) NOT NULL,
-  date_of_ride TIMESTAMP DEFAULT (CURRENT_TIMESTAMP  AT TIME ZONE 'EST'),
+  date_of_ride DATE NOT NULL,
+  time_of_ride TIME NOT NULL,
   cost INTEGER NOT NULL DEFAULT 11,
   available_seats INTEGER NOT NULL,
   pick_up VARCHAR(255) NOT NULL,
   drop_off VARCHAR(255) NOT NULL,
-  description TEXT,
+  ride_description TEXT,
+  ride_image TEXT,  
   is_booked BOOLEAN NOT NULL DEFAULT FALSE,
   is_active BOOLEAN NOT NULL DEFAULT TRUE
   );
--- db/seeds/02_rides.sql
+++ db/seeds/02_rides.sql
@@ -11,7 +11,13 @@
   -- is_booked BOOLEAN NOT NULL DEFAULT FALSE,
   -- is_active BOOLEAN NOT NULL DEFAULT TRUE,DROP TABLE IF EXISTS users CASCADE;
 
-INSERT INTO rides (owner_id,origin,destination,available_seats,pick_up,drop_off) 
-            VALUES (1,'toronto','niagra',3,'toronto','niagra');
-INSERT INTO rides (owner_id,origin,destination,available_seats,pick_up,drop_off) 
-            VALUES (2,'ottawa','downtown',3,'ottawa','downtown');     
+INSERT INTO rides (owner_id,origin,destination,available_seats,pick_up,drop_off,date_of_ride,time_of_ride,ride_image) 
+            VALUES (1,'Toronto, ON, Canada','Niagara Falls, ON, Canada',3,'126,Brook Street','2nd Avenue Niagra Falls City','2022-02-02','8:00:00','https://images.pexels.com/photos/112460/pexels-photo-112460.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500');
+INSERT INTO rides (owner_id,origin,destination,available_seats,pick_up,drop_off,date_of_ride,time_of_ride,ride_image) 
+            VALUES (2,'Hamilton, ON, Canada','Brampton, ON, Canada',3,'126,Brook Street','123,York street','2022-02-02','8:00:00','https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'); 
+INSERT INTO rides (owner_id,origin,destination,available_seats,pick_up,drop_off,date_of_ride,time_of_ride,ride_image) 
+            VALUES (1,'Mississauga, ON, Canada','Niagara Falls, ON, Canada',3,'3rd Avenue WilliStreet','2nd Avenue Niagra Falls City','2022-02-02','8:59:00','https://images.pexels.com/photos/210019/pexels-photo-210019.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500');
+INSERT INTO rides (owner_id,origin,destination,available_seats,pick_up,drop_off,date_of_ride,time_of_ride,ride_image) 
+            VALUES (2,'Churchville, Brampton, ON, Canada','Brampton, ON, Canada',3,'126,Camilo Street','123,Kempstreet','2022-02-02','12:00:00','https://images.pexels.com/photos/116675/pexels-photo-116675.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500');
+INSERT INTO rides (owner_id,origin,destination,available_seats,pick_up,drop_off,date_of_ride,time_of_ride,ride_image) 
+            VALUES (3,'Toronto, ON, Canada','Niagara Falls, ON, Canada',3,'126,Brook Street','2nd Avenue Niagra Falls City','2022-02-03','3:00:00','https://images.pexels.com/photos/112460/pexels-photo-112460.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500');                 
