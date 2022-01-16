2796b67ee22566008b605dae5676edec37501d53
MajorChe <charithnanduri03@gmail.com>
2022-01-15 19:10:02 -0500

    add server side authentication for login route and set cookies

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,21 +6,38 @@ const userfn = require("../db/queries/user");
 
 router.post("/login", (req, res) => {
   validateForm(req, res);
+  userfn.getUser(req.body.username).then(async(result) => {
+    if (result) {
+      const checkPassword = await bcrypt.compare(req.body.password, result.password);
+      if (checkPassword) {
+        req.session.user = {
+          username: req.body.username,
+          id: result.id,
+        };
+        res.json({ loggedIn: true, status: req.body.username });
+      } else {
+        console.log("password doesnt match");
+        res.json({ loggedIn: false, status: "Wrong username or password" });
+      }
+    } else {
+      console.log("Wrong username");
+      res.json({ loggedIn: false, status: "Wrong username or password" });
+    }
+  });
 });
 
 router.post("/register", (req, res) => {
   validateForm(req, res);
-  console.log("hello1");
   userfn.getUser(req.body.username).then(async (result) => {
     if (result) {
       res.json({ loggedIn: false, status: "Username exists!! Please login" });
     } else {
       const hashedpassword = await bcrypt.hash(req.body.password, 12);
-      userfn.postUser(req.body.username, hashedpassword).then((response) => {
+      userfn.postUser(req.body.username, hashedpassword).then((result) => {
         req.session.user = {
           username: req.body.username,
-          id: response.id
-        }
+          id: result.id,
+        };
         res.json({ loggedIn: true, username: req.body.username });
       });
     }
