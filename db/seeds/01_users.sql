c5b08a9170418d21e947adcc4467b3458871e110
MajorChe <charithnanduri03@gmail.com>
2022-01-17 15:30:23 -0500

    remove db folder

--- db/seeds/01_users.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users (email,name,contact,password) VALUES ('alice@mail.com','Alice','900000011','a');
-INSERT INTO users (email,name,contact,password) VALUES ('bentley@mail.com','Bentley','900000022','b');
-INSERT INTO users (email,name,contact,password) VALUES ('calvin@mail.com','Calvin','9000000033','c');
