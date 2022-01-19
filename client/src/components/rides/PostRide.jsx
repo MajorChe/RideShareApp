48a7464a96119e7d22a9f28ee3166ef39f1b8fd2
kaviramsv <kavitasvenkitachalam@gmail.com>
2022-01-18 19:10:51 -0500

    Add post ride component

--- client/src/components/rides/PostRide.jsx
+++ client/src/components/rides/PostRide.jsx
@@ -1,73 +1,125 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import {
-  FormControl,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Stack,
   FormLabel,
-  FormErrorMessage,
-  FormHelperText,
-  Input,
-  NumberInputField,
-  NumberInput,
-  NumberInputStepper,
-  NumberIncrementStepper ,
-  NumberDecrementStepper,
-} from '@chakra-ui/react'; 
+  FormControl,
+  InputGroup,
+} from '@chakra-ui/react';
+import { Button } from "@chakra-ui/react";
+import 'react-datepicker/dist/react-datepicker.css'
+import { useContext } from "react";
+import { AccountContext } from "../hooks/AccountContext";
+import DatePicker from "react-datepicker";
+import Navbar from '../Navbar';
+import TimePicker from "rc-time-picker";
+import 'rc-time-picker/assets/index.css';
+import { Select } from '@chakra-ui/react'
+import Places from './Places';
+import "./Post.css";
+import {useNavigate} from 'react-router-dom';
+
+
 function PostRide() {
-  // INSERT INTO rides (owner_id,origin,destination,available_seats,pick_up,drop_off,date_of_ride,time_of_ride,ride_image) 
-  // VALUES (3,'Toronto, ON, Canada','Niagara Falls, ON, Canada',3,'126,Brook Street','2nd Avenue Niagra Falls City','2022-02-03','3:00
-  // :00','https://images.pexels.com/photos/112460/pexels-photo-112460.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500');                 
-
-  const [owner_id, setOwnerId] = useState(1);
-  const [origin, setOrigin] = useState("");
-  const [destination, setDestination] = useState("");
-  const [available_seats, setAvailable_seats] = useState("");
-  const [date, setDate] = useState("");
-  const [time, setTime] = useState("");
+  const navigate = useNavigate();
+  const { user } = useContext(AccountContext);
+  const [seats, setSeats] = useState("1");
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedTime, setSelectedTime] = useState('');
   const [image, setImage] = useState("");
+  const [address1, setAddress1] = useState("");
+  const [address2, setAddress2] = useState("");
 
-  // useEffect(() => {
-  //   axios.put("http://localhost:8000/ride",
-  //     {
-  //       params:
-  //       {
-  //         id: ,
-  //       }
-  //     }).then((res) => {
-  //       console.log("rides", res.data);
-  //       setRides(res.data)
-  //     });
+  console.log(selectedDate); 
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  // }, []);
+    var month = selectedDate.getUTCMonth() + 1; //months from 1-12
+    var day = selectedDate.getUTCDate();
+    var year = selectedDate.getUTCFullYear();
+    let format = year + "-" + month + "-" + day;
+   
+    axios.post("/postRide", {
 
-  const [input, setInput] = useState('')
-
-  const handleInputChange = (e) => setInput(e.target.value)
-
-  const isError = input === '';
-
+      params:
+      {
+        owner_id: user.id,
+        origin: address1,
+        destination: address2,
+        available_seats: seats,
+        date_of_ride: format,
+        time_of_ride: " 23:35:44",
+        ride_image: image,
+      }
+    })
+      .then((res) => {
+        if (!res) return;
+        console.log(res.data);
+        return res.data;
+      })
+  }
+  function updateAdress1(address1) {
+    setAddress1(address1);
+    console.log(address1);
+  }
+  function updateAdress2(address2) {
+    setAddress2(address2);
+    console.log(address2);
+  }
   return (
+    <>
+      <Navbar />
+      <div id="details">
+        <form onSubmit={handleSubmit} class="form-horizontal">
+          <FormControl >
+            <Stack spacing={5}>
 
-    <FormControl isInvalid={isError} isRequired>
-      <FormLabel htmlFor='email'>Email</FormLabel>
-      <Input id='email' type='email' value={input} onChange={handleInputChange} />
-      {!isError ? (
-        <FormHelperText>
-          Enter the email you'd like to receive the newsletter on.
-        </FormHelperText>
-      ) : (
-        <FormErrorMessage>Email is required.</FormErrorMessage>
-      )}
-      <NumberInput max={6} min={1}>
-        <NumberInputField id='amount' />
-        <NumberInputStepper>
-          <NumberIncrementStepper />
-          <NumberDecrementStepper />
-        </NumberInputStepper>
-      </NumberInput>
+              <Places updateAdress={updateAdress1} adresss1={address1} location={"from"} />
+              <Places updateAdress={updateAdress2} adresss1={address2} location={"to"} />
+              <InputGroup>
+                <FormLabel>Date</FormLabel>
+                <DatePicker selected={selectedDate} onChange={date => setSelectedDate(date)} minDate={new Date()} />
+              </InputGroup>
+              <InputGroup>
+                <FormLabel>Seats</FormLabel>
+                <Menu>
+                  {({ isOpen }) => (
+                    <>
+                      <MenuButton isActive={isOpen} as={Button} >
+                        {isOpen ? seats : seats}
+                      </MenuButton>
+                      <MenuList>
+                        <MenuItem onClick={() => setSeats(1)}>1</MenuItem>
+                        <MenuItem onClick={() => setSeats(2)}>2</MenuItem>
+                        <MenuItem onClick={() => setSeats(3)}>3</MenuItem>
+                        <MenuItem onClick={() => setSeats(4)}>4</MenuItem>
+                      </MenuList>
+                    </>
+                  )}
+                </Menu>
+              </InputGroup>
 
+              <TimePicker
+                placeholder="Select Time"
+                use24Hours
+                showSecond={true}
+                focusOnOpen={true}
+                format="hh:mm"
+                onChange={e => setSelectedTime(e.format('HH:mm:ss'))}
+              />
 
-    </FormControl>
-
+            </Stack>
+            <div class="col-12">
+              <button type="submit" class="btn btn-primary">Post</button>
+            </div>
+          </FormControl >
+        </form>
+      </div>
+    </>
 
 
   );
