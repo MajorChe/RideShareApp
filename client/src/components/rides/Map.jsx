d7b89d149c6176b6a73711aa24431fcba85030f9
MajorChe <charithnanduri03@gmail.com>
2022-01-22 14:40:30 -0500

    add current location function

--- client/src/components/rides/Map.jsx
+++ client/src/components/rides/Map.jsx
@@ -21,7 +21,6 @@ import { CalendarIcon } from "@chakra-ui/icons";
 import "react-datepicker/dist/react-datepicker.css";
 import DatePicker from "react-datepicker";
 
-
 const Map = (props) => {
   console.log(props);
   const [distance, setDistance] = useState(0);
@@ -107,22 +106,78 @@ const Map = (props) => {
   const isError1 = address === "";
   const isError2 = address2 === "";
 
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        getCordinates,
+        handleLocationErrors
+      );
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  };
+  const handleLocationErrors = (error) => {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        alert(
+          "User denied the request for Geolocation.Please enter the adress manually"
+        );
+        break;
+      case error.POSITION_UNAVAILABLE:
+        alert(
+          "Location information is unavailable.Please enter the adress manually"
+        );
+        break;
+      case error.TIMEOUT:
+        alert(
+          "The request to get user location timed out.Please enter the adress manually"
+        );
+        break;
+      case error.UNKNOWN_ERROR:
+        alert("An unknown error occurred.Please enter the adress manually");
+        break;
+      default:
+        alert("An unknown error occurred.Please enter the adress manually");
+    }
+  };
+  const reverseGeocode = (latitude, longitude) => {
+    fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&sensor=false&key=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("result", data.results[0].formatted_address);
+        setAddress(data.results[0].formatted_address);
+      })
+      .catch((err) => alert("error"));
+  };
+  const getCordinates = (position) => {
+    console.log("position", position);
+    let lat = position.coords.latitude;
+    let lng = position.coords.longitude;
+    reverseGeocode(lat, lng);
+  };
+
   return (
-        <>
-        <Flex justifyContent={"space-around"} alignItems={"center"}>
-          <HStack mt={"100px"}>
-            <VStack>
-          <PlacesAutocomplete
-            value={address}
-            onChange={setAddress}
-            onSelect={handleSelect}
-          >
-            {({
-              getInputProps,
-              suggestions,
-              getSuggestionItemProps,
-              loading,
-            }) => (
+    <>
+      <Flex
+        justifyContent={"space-evenly"}
+        alignItems={"center"}
+        bg="blackAlpha.200"
+      >
+        <HStack mt={"100px"}>
+          <VStack>
+            <PlacesAutocomplete
+              value={address}
+              onChange={setAddress}
+              onSelect={handleSelect}
+            >
+              {({
+                getInputProps,
+                suggestions,
+                getSuggestionItemProps,
+                loading,
+              }) => (
                 <FormControl isInvalid={isError1}>
                   <Input
                     value={props.address1}
@@ -134,105 +189,113 @@ const Map = (props) => {
 
                     {suggestions.map((suggestion) => {
                       const style = {
-                        backgroundColor: suggestion.active ? "#41b6e6" : "#fff",
+                        backgroundColor: suggestion.active ? "teal" : "#fff",
+                        color: suggestion.active ? "white" : "black",
                       };
 
                       return (
-                        <chakra.div {...getSuggestionItemProps(suggestion, { style })}>
+                        <chakra.div
+                          {...getSuggestionItemProps(suggestion, { style })}
+                        >
                           {suggestion.description}
                         </chakra.div>
                       );
                     })}
                   </chakra.div>
                 </FormControl>
-            )}
-          </PlacesAutocomplete>
-          <br />
-          <PlacesAutocomplete
-            value={address2}
-            onChange={setAddress2}
-            onSelect={handleSelect2}
-          >
-            {({
-              getInputProps,
-              suggestions,
-              getSuggestionItemProps,
-              loading,
-            }) => (
-              <chakra.div>
-                <FormControl isInvalid={isError2}>
-                  <Input
-                    class="form-control"
-                    {...getInputProps({ placeholder: "To" })}
-                    required
-                  />
-                  <chakra.div>
-                    {loading ? <chakra.div>...loading</chakra.div> : null}
+              )}
+            </PlacesAutocomplete>
+            <br />
+            <PlacesAutocomplete
+              value={address2}
+              onChange={setAddress2}
+              onSelect={handleSelect2}
+            >
+              {({
+                getInputProps,
+                suggestions,
+                getSuggestionItemProps,
+                loading,
+              }) => (
+                <chakra.div>
+                  <FormControl isInvalid={isError2}>
+                    <Input
+                      class="form-control"
+                      {...getInputProps({ placeholder: "To" })}
+                      required
+                    />
+                    <chakra.div>
+                      {loading ? <chakra.div>...loading</chakra.div> : null}
 
-                    {suggestions.map((suggestion) => {
-                      const style = {
-                        backgroundColor: suggestion.active ? "#41b6e6" : "#fff",
-                      };
+                      {suggestions.map((suggestion) => {
+                        const style = {
+                          backgroundColor: suggestion.active ? "teal" : "#fff",
+                          color: suggestion.active ? "white" : "black",
+                        };
 
-                      return (
-                        <chakra.div {...getSuggestionItemProps(suggestion, { style })}>
-                          {suggestion.description}
-                        </chakra.div>
-                      );
-                    })}
-                  </chakra.div>
-                </FormControl>
-              </chakra.div>
-            )}
-          </PlacesAutocomplete>
-          <br />
-          <Stack direction="column">
-            <Button as={DatePicker}
-              placeholderText="Date"
-              selected={props.selectedDate}
-              onChange={(date) => props.updateSelectedDate(date)}
-              minDate={new Date()}
-              colorScheme='black' variant='outline'
-              >
+                        return (
+                          <chakra.div
+                            {...getSuggestionItemProps(suggestion, { style })}
+                          >
+                            {suggestion.description}
+                          </chakra.div>
+                        );
+                      })}
+                    </chakra.div>
+                  </FormControl>
+                </chakra.div>
+              )}
+            </PlacesAutocomplete>
+            <br />
+            <Stack direction="column">
+              <Button
+                as={DatePicker}
+                placeholderText="Date"
+                selected={props.selectedDate}
+                onChange={(date) => props.updateSelectedDate(date)}
+                minDate={new Date()}
+                colorScheme="black"
+                variant="outline"
+              ></Button>
+              <HStack>
+                <Text>Show exact matches</Text>
+                <Checkbox
+                  type="checkbox"
+                  checked={props.only}
+                  onChange={handleChange}
+                />
+              </HStack>
+            </Stack>
+            <Button onClick={getLocation}>
+             Use Current Location
             </Button>
-            <HStack>
-              <Text>Show exact matches</Text>
-              <Checkbox
-                type="checkbox"
-                checked={props.only}
-                onChange={handleChange}
-              />
-            </HStack>
-          </Stack>
-          <button id="submit" class="btn btn-dark" onClick={onSubmit}>
-            Search
-          </button>
+            <Button id="submit" onClick={onSubmit}>
+              Search
+            </Button>
+            <br />
+            <br /> <br />
+            <br />
+            <p>
+              Distance: {distance} Duration: {duration}
+            </p>
+          </VStack>
+        </HStack>
+        <div id="map" style={{ height: "300px" }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{
+              key: process.env.REACT_APP_API_KEY,
+            }}
+            defaultCenter={{ lat: 40.756795, lng: -73.954298 }}
+            defaultZoom={10}
+            yesIWantToUseGoogleMapApiInternals
+            onGoogleApiLoaded={({ map, maps }) => onLoad(map, maps)}
+          ></GoogleMapReact>
           <br />
-          <br /> <br />
-          <br />
-          <p>
-            Distance: {distance} Duration: {duration}
-          </p>
+        </div>
         {/* </div> */}
-      {/* </div> */}
-      </VStack>
-      </HStack>
-      <div id="map" style={{ height: "300px" }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{
-            key: process.env.REACT_APP_API_KEY,
-          }}
-          defaultCenter={{ lat: 40.756795, lng: -73.954298 }}
-          defaultZoom={10}
-          yesIWantToUseGoogleMapApiInternals
-          onGoogleApiLoaded={({ map, maps }) => onLoad(map, maps)}
-        ></GoogleMapReact>
-        <br />
-      </div>
-      {/* </div> */}
       </Flex>
-      </>
+    </>
   );
-}
+};
 
 export default Map;
