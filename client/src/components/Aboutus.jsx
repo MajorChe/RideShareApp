dfe86415acb62af68fa1d0b98080c5fd1f349930
MajorChe <charithnanduri03@gmail.com>
2022-01-25 18:23:06 -0500

    style services component

--- client/src/components/Aboutus.jsx
+++ client/src/components/Aboutus.jsx
@@ -1,28 +1,44 @@
-import { Box, Flex, Text, Heading, HStack, Link, Icon, AspectRatio, VStack } from "@chakra-ui/react";
+import { Box, Flex, Text, Heading, HStack, Link, Icon, AspectRatio, VStack, SimpleGrid } from "@chakra-ui/react";
 import { FiArrowUpRight } from "react-icons/fi";
 import carshare from "../assets/carshare.png";
 export const Aboutus = () => {
   return (
-    <Box  w="full" bg="#3D9AD5" px="200px"  h={"500px"} >
-      <Flex justifyContent="space-between" alignItems="center" >
-        <Box w={"800px"}>
-        <Flex direction={"column"} >
-        <Heading  fontSize={60} ml={"110px"} letterSpacing="4px" mb={"30"} color="white">
-         About Us
+    <Box p={4}  mb={"100px"} bg="#3D9AD5">
+      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10} mt={"70px"}>
+        <Flex direction={"column"}>
+        <Heading  fontSize={50} ml={"110px"} letterSpacing="4px" mb={"30"} color="white">
+          ABOUT US
         </Heading>
 
-        <Text fontSize={20} pl={"150px"} color="whiteAlpha.700" pb="20px" w={"400px"}> 
-            Proident aliquip elit nostrud dolore cupidatat id reprehenderit
-            magna enim ullamco eu consequat labore. Laboris irure ea pariatur
-           
+        <Text fontSize={20} pl={"150px"} color={'gray.600'} pb="20px" w={"400px"}> 
+          Proident aliquip elit nostrud dolore cupidatat id reprehenderit
+          magna enim ullamco eu consequat labore. Laboris irure ea pariatu 
         </Text>
         </Flex>
-        </Box>
-        <AspectRatio w="800px" ratio={4/3}> 
-        <img borderRadius="md" ml={"100px"} src={carshare} alt="car"/>
-      </AspectRatio>
+      </SimpleGrid>
+    </Box>
+    )
+  };
+    
+    // <Box  w="full" bg="#3D9AD5" px="200px"  h={"500px"} >
+    //   <Flex justifyContent="space-between" alignItems="center" >
+    //     <Box w={"800px"}>
+    //     <Flex direction={"column"} >
+    //     <Heading  fontSize={60} ml={"110px"} letterSpacing="4px" mb={"30"} color="white">
+    //      About Us
+    //     </Heading>
+
+    //     <Text fontSize={20} pl={"150px"} color="whiteAlpha.700" pb="20px" w={"400px"}> 
+    //         Proident aliquip elit nostrud dolore cupidatat id reprehenderit
+    //         magna enim ullamco eu consequat labore. Laboris irure ea pariatur
+           
+    //     </Text>
+    //     </Flex>
+    //     </Box>
+    //     <AspectRatio w="800px" ratio={4/3}> 
+    //     <img borderRadius="md" ml={"100px"} src={carshare} alt="car"/>
+    //   </AspectRatio>
       
-      </Flex>
+    //   </Flex>
       
-      </Box>
-  )}
+    //   </Box>
--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,139 +1,76 @@
-import { Box, Flex, Text, Heading, HStack, Link, Icon } from "@chakra-ui/react";
-import React from "react";
-// import { ServiceCard } from "../../../components/serviceCard";
-import { RiComputerLine } from "react-icons/ri";
-import { BsCardChecklist, BsPencilSquare } from "react-icons/bs";
-import { FiArrowUpRight } from "react-icons/fi";
+import { Box, SimpleGrid, Icon, Text, Stack, Flex, Heading, Link } from '@chakra-ui/react';
 import {AiFillCar, AiOutlineFileSearch} from "react-icons/ai";
-import {MdLocalPostOffice} from "react-icons/md";
-import {BsJournalBookmark} from "react-icons/bs";
-import {BsSignpost} from "react-icons/bs";
+import {BsJournalBookmark, BsSignpost, BsCardChecklist} from "react-icons/bs";
 import {MdOutlineMobileFriendly} from "react-icons/md";
 import { FaMapMarkerAlt } from "react-icons/fa";
+import { FiArrowUpRight } from 'react-icons/fi';
 
-export const Services = () => {
+const Feature = ({ title, text, icon1, icon2 }) => {
   return (
-    <Box w="full" py="60px" px="200px" >
-      <Flex justifyContent="space-between" alignItems="center" pb="60px">
-        <Heading  fontSize={60}  ml={"110px"} letterSpacing="4px" color="#3D9AD5">
-          We Help You 
-        </Heading>
+    <Stack spacing={3} bg="yellow.50" padding={"40px"}>
+      <Flex
+        w={16}
+        h={16}
+        alignSelf={"center"}
+        align={'center'}
+        justify={'center'}
+        color={'white'}
+        rounded={'full'}
+        mb={1}>
+        {icon1}
+        {icon2}
       </Flex>
-      <HStack w="full" spacing="60px"  ml={"100px"}>      
-        <Flex
-          flexDirection="column"
-          bg="yellow.50"
-          p="40px"
-          w="full"
-          height="380px"
-          justifyContent="space-between"
-        >
-          <HStack>
-          <Icon color="#3D9AD5" h={20} w={20} as={AiOutlineFileSearch}/>
-          <Icon color="#3D9AD5" h={10} w={10} as={AiFillCar}/>
-          </HStack>
-          <Box>
-            <Heading color="#3D9AD5" fontSize={28} letterSpacing="3px" pb="20px">
-            Find Rides 
-            </Heading>            
-          </Box>
-          <Link color="#3D9AD5">
-            Explore more
-            <Icon as={FiArrowUpRight} ml="10px" h={5} w={5} />
-          </Link>
-        </Flex>
-
-          
-        <Flex
-          flexDirection="column"
-          bg="yellow.50"
-          p="40px"
-          w="full"
-          height="380px"
-          justifyContent="space-between"
-        > 
-        <HStack>
-          <Icon color="#3D9AD5" h={20} w={20} as={BsSignpost}/>
-          <Icon color="#3D9AD5" h={10} w={10} as={BsCardChecklist}/>
-          </HStack>
-          <Box>
-            <Heading color="#3D9AD5" fontSize={28} letterSpacing="3px" pb="20px">
-              Post Rides 
-            </Heading>           
-          </Box>
-          <Link color="#3D9AD5">
-            Explore more
-            <Icon as={FiArrowUpRight} ml="10px" h={5} w={5} />
-          </Link>
-        </Flex>
-
-
-        <Flex
-          flexDirection="column"
-          bg="yellow.50"
-          p="40px"
-          w="full"
-          height="380px"
-          justifyContent="space-between"
-         >
-          <Icon color="#3D9AD5" h={20} w={20} as={BsJournalBookmark}/>
-          <Box>
-            <Heading color="#3D9AD5" fontSize={28} letterSpacing="3px" pb="20px">
-              Book Rides
-            </Heading>            
-          </Box>
-          <Link color="#3D9AD5">
-            Explore more
-            <Icon as={FiArrowUpRight} ml="10px" h={5} w={5} />
-          </Link>
-        </Flex>
-
-        <Flex
-          flexDirection="column"
-          bg="yellow.50"
-          p="40px"
-          w="full"
-          height="380px"
-          justifyContent="space-between"
-         >
-          <Icon color="#3D9AD5" h={20} w={20} as={MdOutlineMobileFriendly}/>
-          <Box>
-            <Heading color="#3D9AD5" fontSize={28} letterSpacing="3px" pb="20px">
-              Get Notified
-            </Heading>            
-          </Box>
-          <Link color="#3D9AD5">
-            Explore more
-            <Icon as={FiArrowUpRight} ml="10px" h={5} w={5} />
-          </Link>
-        </Flex>
-        
-
-        <Flex
-          flexDirection="column"
-          bg="yellow.50"
-          p="40px"
-          w="full"
-          height="380px"
-          justifyContent="space-between"
-         >
-          <Icon color="#3D9AD5" h={20} w={20} as={FaMapMarkerAlt}/>
-          <Box>
-            <Heading color="#3D9AD5" fontSize={28} letterSpacing="3px" pb="20px">
-             Reach Safe
-            </Heading>            
-          </Box>
-          <Link color="#3D9AD5">
-            Explore more
-            <Icon as={FiArrowUpRight} ml="10px" h={5} w={5} />
-          </Link>
-        </Flex>     
-         
-
-      </HStack>   
-      </Box>
- 
-      
-    
+      <Text fontWeight={600} fontSize={"xl"} alignSelf={"center"}>{title}</Text>
+      <Text color={'gray.600'} align={"center"}>{text}</Text>
+      <Text as={Link} align={"center"}>Explore more <Icon as={FiArrowUpRight} ml="10px" h={5} w={5} /></Text>
+    </Stack>
   );
 };
+
+export function Services() {
+  return (
+    <Box p={4} mt={"200px"} mb={"70px"}>
+      <Heading align={"center"} color={"blue.600"}>USING RIDE SHARE ?</Heading>
+      <SimpleGrid columns={{ base: 1, md: 5 }} spacing={10} mt={"70px"}>
+        
+        <Feature
+          icon1={<Icon color="#3D9AD5" h={20} w={20} mr={2} as={AiOutlineFileSearch}/>}
+          icon2={<Icon color="#3D9AD5" h={10} w={10} as={AiFillCar}/>}
+          title={'FIND RIDES'}
+          text={
+            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
+          }
+        />
+        <Feature
+          icon1={<Icon color="#3D9AD5" h={20} w={20} as={BsSignpost}/>}
+          icon2={<Icon color="#3D9AD5" h={10} w={10} as={BsCardChecklist}/>}
+          title={'POST RIDES'}
+          text={
+            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
+          }
+        />
+        <Feature
+          icon1={<Icon color="#3D9AD5" h={20} w={20} as={BsJournalBookmark}/>}
+          title={'BOOK RIDES'}
+          text={
+            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
+          }
+        />
+        <Feature
+          icon1={<Icon color="#3D9AD5" h={20} w={20} as={MdOutlineMobileFriendly}/>}
+          title={'GET NOTIFIED'}
+          text={
+            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
+          }
+        />
+        <Feature
+          icon1={<Icon color="#3D9AD5" h={20} w={20} as={FaMapMarkerAlt}/>}
+          title={'REACH SAFE'}
+          text={
+            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
+          }
+        />
+      </SimpleGrid>
+    </Box>
+  );
+}
